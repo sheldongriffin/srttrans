9813ab9226aca29899110be37c22ccd391665aa5
Sheldon Griffin <chrisdavidgriffin@yahoo.com>
2012-02-02 11:22:00 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿using System.Windows.Forms;
+
+namespace SrtTrans
+{
+    partial class Form1
+    {
+        private ListBox lbTraditional;
+        private ListBox lbSimplified;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox tbMovie;
+        private TextBox tbSubtitle;
+        private Button button3;
+        private ListBox lbUTF8;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSimplified = new System.Windows.Forms.ListBox();
+            this.lbTraditional = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMovie = new System.Windows.Forms.TextBox();
+            this.tbSubtitle = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lbUTF8 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbSimplified
+            // 
+            this.lbSimplified.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSimplified.Location = new System.Drawing.Point(6, 60);
+            this.lbSimplified.Name = "lbSimplified";
+            this.lbSimplified.Size = new System.Drawing.Size(246, 316);
+            this.lbSimplified.TabIndex = 0;
+            // 
+            // lbTraditional
+            // 
+            this.lbTraditional.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTraditional.FormattingEnabled = true;
+            this.lbTraditional.Location = new System.Drawing.Point(258, 60);
+            this.lbTraditional.Name = "lbTraditional";
+            this.lbTraditional.Size = new System.Drawing.Size(246, 316);
+            this.lbTraditional.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(87, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "简体";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(332, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "繁体";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Subtitles";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Movie";
+            // 
+            // tbMovie
+            // 
+            this.tbMovie.Location = new System.Drawing.Point(98, 34);
+            this.tbMovie.Name = "tbMovie";
+            this.tbMovie.Size = new System.Drawing.Size(559, 20);
+            this.tbMovie.TabIndex = 10;
+            // 
+            // tbSubtitle
+            // 
+            this.tbSubtitle.Location = new System.Drawing.Point(98, 8);
+            this.tbSubtitle.Name = "tbSubtitle";
+            this.tbSubtitle.Size = new System.Drawing.Size(559, 20);
+            this.tbSubtitle.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(593, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "UTF8";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbUTF8
+            // 
+            this.lbUTF8.FormattingEnabled = true;
+            this.lbUTF8.Location = new System.Drawing.Point(510, 60);
+            this.lbUTF8.Name = "lbUTF8";
+            this.lbUTF8.Size = new System.Drawing.Size(246, 316);
+            this.lbUTF8.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(766, 426);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lbUTF8);
+            this.Controls.Add(this.tbSubtitle);
+            this.Controls.Add(this.tbMovie);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbTraditional);
+            this.Controls.Add(this.lbSimplified);
+            this.Name = "Form1";
+            this.Text = "Drop Files Demo";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
+
+namespace SrtTrans
+{
+    public partial class Form1 : Form
+    {
+        private string moviePath;
+        private string traditionalConverted;
+        private string simplifiedConverted;
+        private string utf8converted;
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+
+            if (args != null)
+            {
+                OpenFiles(args);
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Run(new Form1(args));
+        }
+
+        public void OpenFiles(Array a)
+        {
+            string sError = "";
+
+            // process all files in array
+            for (int i = 0; i < a.Length; i++)
+            {
+                string sFile = a.GetValue(i).ToString();
+
+                FileInfo info = new FileInfo(sFile);
+
+                if (!info.Exists)
+                {
+                    sError += "\nIncorrect file name: " + sFile;
+                }
+                else if (info.Name.ToLower().IndexOf(".srt") > -1)
+                {
+                    tbSubtitle.Text = info.Name;
+                    ShowGuess(sFile);
+                }
+                else if (info.Name.ToLower().IndexOf(".mkv") > -1)
+                {
+                    tbMovie.Text = info.Name;
+                    moviePath = info.FullName;
+                }
+            }
+
+            if (sError.Length > 0)
+                MessageBox.Show(this, sError, "Open File Error");
+        }
+
+        public void ShowGuess(string sFile)
+        {
+            byte[] bytes = File.ReadAllBytes(sFile);
+            traditionalConverted = Encoding.GetEncoding(950).GetString(bytes);
+            simplifiedConverted = Encoding.GetEncoding(936).GetString(bytes);
+            utf8converted = Encoding.UTF8.GetString(bytes);
+
+            AddTextPreview(lbTraditional, traditionalConverted);
+            AddTextPreview(lbSimplified, simplifiedConverted);
+            AddTextPreview(lbUTF8, utf8converted);
+        }
+
+        private static void AddTextPreview(ListBox lb, string str)
+        {
+            lb.Items.Clear();
+            string[] lines = str.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            int i = 0;
+            foreach (string s in lines.Where(t => !string.IsNullOrWhiteSpace(t) && !System.Text.RegularExpressions.Regex.IsMatch(t, @"^\d")))
+            {
+                if (i > 30)
+                {
+                    break;
+                }
+
+                lb.Items.Add(s);
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileInfo movie = new FileInfo(moviePath);
+            string srtPathName = movie.DirectoryName + @"\" + movie.Name.Replace(".mkv", ".srt");
+            File.WriteAllText(srtPathName, traditionalConverted);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileInfo movie = new FileInfo(moviePath);
+            string srtPathName = movie.DirectoryName + @"\" + movie.Name.Replace(".mkv", ".srt");
+            File.WriteAllText(srtPathName, utf8converted);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo movie = new FileInfo(moviePath);
+            string srtPathName = movie.DirectoryName + @"\" + movie.Name.Replace(".mkv", ".srt");
+            File.WriteAllText(srtPathName, Microsoft.VisualBasic.Strings.StrConv(simplifiedConverted, Microsoft.VisualBasic.VbStrConv.TraditionalChinese));
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array a = (Array)e.Data.GetData(DataFormats.FileDrop);
+
+                if (a != null)
+                {
+                    this.BeginInvoke((Action)(() => OpenFiles(a)));
+                    this.Activate();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
